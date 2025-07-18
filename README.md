508e8d8dd406acd7eab4d9ad6cf87fdc3a010c2f
Tatarin116rus <youngerxabi.aidar@gmail.com>
2025-07-18 19:48:10 +0300

    финальный макет

---
+++ README.md
@@ -0,0 +1,5 @@
+Практическая задача: "Основная информация. Настройки окружения + верстка финального макета"
+
+1.GitHub Pages:
+
+2.Макет: https://www.figma.com/design/ttlPwrOE9PxIpxl3FqkIT1/Верстка-приемлемого-макета?node-id=0-312&p=f&t=a4LaRHJXGrvlgqJj-0
---
+++ src/js/swiper.js
@@ -0,0 +1,28 @@
+export function initSwiper(containerSelector) {
+  const newSwiper = new Swiper(containerSelector, {
+    loop: true,
+    slidesPerView: "auto",
+    spaceBetween: 0,
+    slideToClickedSlides: true,
+    pagination: {
+      el: containerSelector.querySelector(".swiper-pagination"),
+      clickable: true,
+    },
+    grabCursor: true,
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+      pageUpDown: true, 
+    },
+    slideOverflow: true,
+    allowTouchMove: true,
+  });
+
+  return newSwiper;
+}
+
+export function destroySwiper(swiper) {
+  if (swiper && typeof swiper.destroy === "function") {
+    swiper.destroy(true, true); // параметры: deleteInstance и cleanStyles
+  }
+}
